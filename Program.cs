359532e04c0ca7f4735d03d8d8db02c28106feb7
AlekhinVladimir <Alehin.v.m@mail.com>
2023-01-30 12:15:30 +0400

    printing method and output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = object; i < array.Length - 1; i++)
+    {
+        Console.Write($"'{array[i]}', ");
+    }
+    Console.Write($"'{array[array.Length - 1]}']");
+}
+
+string[] myArray = { "death", "life", "-_-", ">_<", "3579", "123", "bom" };
+PrintArray(FilterArray(myArray));
