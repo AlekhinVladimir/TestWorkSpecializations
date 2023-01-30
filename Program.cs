48472d3462f5f637e20f4302afea6c237044df0d
AlekhinVladimir <Alehin.v.m@mail.com>
2023-01-30 12:02:48 +0400

    main method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равно 3 символам.
+
+
+string[] FilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+         if(array[i].Length<=3)
+         {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
